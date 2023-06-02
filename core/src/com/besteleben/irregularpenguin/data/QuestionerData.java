d6d8ebfd88fe72f8baaac8fbbe157cbe048f79a4
aYudizzle <sascha@beste-leben.de>
2023-06-03 00:47:18 +0200

    Working version with the first raw version of input handling

---
+++ core/src/com/besteleben/irregularpenguin/data/QuestionerData.java
@@ -0,0 +1,41 @@
+package com.besteleben.irregularpenguin.data;
+
+import com.besteleben.irregularpenguin.entities.character.questioner.QuestionerGhostColor;
+/** object to transport data from service to controller. */
+public class QuestionerData {
+    /** color of the ghost and the corresponding textures */
+    private QuestionerGhostColor color;
+    /** the verb which should be asked for */
+    private String verb;
+    /** constructor for creating the transfer object */
+    public QuestionerData(QuestionerGhostColor color, String verb){
+        this.color = color;
+        this.verb = verb;
+    }
+
+    /**
+     * Gets color.
+     *
+     * @return value of color
+     */
+    public QuestionerGhostColor getColor() {
+        return color;
+    }
+
+    /**
+     * Gets verb.
+     *
+     * @return value of verb
+     */
+    public String getVerb() {
+        return verb;
+    }
+
+    @Override
+    public String toString() {
+        return "QuestionerData{" +
+                "color=" + color +
+                ", verb='" + verb + '\'' +
+                '}';
+    }
+}
